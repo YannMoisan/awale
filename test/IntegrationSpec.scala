800e489bffd701b9ca6cd81a4fa58b39e843fabf
Yann Moisan <yamo93@gmail.com>
2015-10-05 21:17:14 +0200

    Add build number to sauce labs tests

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -45,6 +45,7 @@ trait EnvAwareDriver {
       caps.setCapability("platform", "Windows 7")
       caps.setCapability("version", "38.0")
       caps.setCapability("tunnelIdentifier", System.getenv("TRAVIS_JOB_NUMBER"))
+      caps.setCapability("build", System.getenv("TRAVIS_BUILD_NUMBER"))
       new RemoteWebDriver(new URL("http://yamo93:c1783a7f-802a-41b5-af11-6c6d1841851e@ondemand.saucelabs.com:80/wd/hub"), caps)
     }
   }
