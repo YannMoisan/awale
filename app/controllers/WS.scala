57faaf309557b81a0a2449e4d76e85210a7352e8
Yann Moisan <yamo93@gmail.com>
2015-10-07 18:15:28 +0200

    refactor : introduce ADT for representing game status

--- app/controllers/WS.scala
+++ app/controllers/WS.scala
@@ -4,14 +4,11 @@ import java.time.{LocalDateTime, Duration}
 
 import akka.actor._
 import akka.event.LoggingReceive
-import play.api.libs.concurrent._
 import play.api.mvc._
 import play.api.Play.current
 import akka.actor.ActorLogging
 
 object MyController extends Controller {
-  // var for testing purpose
-  //var supervisor = Akka.system.actorOf(Props[SupervisorActor])
 
   def socket = WebSocket.acceptWithActor[String, String] { request => out =>
     ReceiverActor.props(out)
@@ -58,17 +55,19 @@ class ReceiverActor(out: ActorRef) extends Actor {
 class SupervisorActor extends Actor with ActorLogging {
   var members : scala.collection.mutable.ArrayBuffer[Player] = scala.collection.mutable.ArrayBuffer()
   var games : scala.collection.mutable.Map[String, Game] = scala.collection.mutable.Map()
+
   def nbPlayers = members.size
-  def nbGames = games.map{ case(_, Game(_, p1, p2, _, _, _, _)) => if (p2.isDefined) 1 else 0}.sum
+  def nbGames = games.collect{ case (_, g: ReadyGame) => g}.size
 
   def receive = LoggingReceive {
     case Close(playerId) => {
-      val maybeGame = games.find(p => p._2.player1.playerId == playerId || p._2.player2.map(_.playerId) == Some(playerId))
-      maybeGame match {
-        case Some((_, Game(_, p1, p2, _, _, _, _))) if p1.playerId == playerId => p2.get.out ! "close"
-        case Some((_, Game(_, p1, p2, _, _, _, _))) => p1.out ! "close"
-        case None =>
+      // find opponents, to inform them
+      val opponents = games.collect{
+        case (_, ReadyGame(_, Player(playerId, _, _), p2, _, _, _, _)) => p2
+        case (_, ReadyGame(_, p1, Player(playerId, _, _), _, _, _, _)) => p1
       }
+      opponents.foreach { _.out! "close"}
+
       members.find(p => p.playerId == playerId).foreach(p=>members.-=(p))
       members.foreach(_.out ! s"Stats:$nbPlayers:$nbGames")
     }
@@ -80,34 +79,38 @@ class SupervisorActor extends Actor with ActorLogging {
     }
 
     case Join(member, gameId) => {
-      val currentGame = games(gameId)
-      val updatedGame = currentGame.join(member)
-      games.put(gameId, updatedGame)
-      updatedGame.player1.out ! ("join1")
-      updatedGame.player1.out ! (s"active")
-      updatedGame.player2.get.out ! ("join2")
-      updatedGame.player2.get.out ! (s"passive")
+      games(gameId) match {
+        case g: PreGame =>
+          val updatedGame = g.join(member)
+          games.put(gameId, updatedGame)
+          updatedGame.player1.out ! ("join1")
+          updatedGame.player1.out ! (s"active")
+          updatedGame.player2.out ! ("join2")
+          updatedGame.player2.out ! (s"passive")
+        case _: ReadyGame => // we can't join a ReadyGame
+      }
 
       members.foreach(_.out ! s"Stats:$nbPlayers:$nbGames")
-
     }
 
     case Create(member) => {
-      log.debug("supervisor.create{}")
-      println("supervisor.create")
       val gameId = Random.nextId
-      games.put(gameId, Game(gameId, member, None, 1, LocalDateTime.now, Duration.ZERO, Duration.ZERO))
+      games.put(gameId, PreGame(gameId, member))
       member.out ! (s"Game:${gameId}")
 
       members.foreach(_.out ! s"Stats:$nbPlayers:$nbGames")
     }
 
     case Move(gameId, playerId, sowId) => {
-      val updatedGame = games(gameId).move(sowId)
-      games.put(gameId, updatedGame)
-      val durations = s":${updatedGame.p1Duration}:${updatedGame.p2Duration}"
-      updatedGame.player1.out ! (if (updatedGame.currentPlayer == 1) "active:"+sowId+durations else "passive" + durations)
-      updatedGame.player2.get.out ! (if (updatedGame.currentPlayer == 2) "active:"+sowId+durations else "passive" + durations)
+      games(gameId) match {
+        case game: ReadyGame =>
+          val updatedGame = game.move(sowId)
+          games.put(gameId, updatedGame)
+          val durations = s":${updatedGame.p1Duration.getSeconds}:${updatedGame.p2Duration.getSeconds}"
+          updatedGame.activePlayer.out ! "active:" + sowId + durations
+          updatedGame.passivePlayer.out ! "passive" + durations
+        case _: PreGame => // we can't move a PreGame
+      }
     }
   }
 }
@@ -125,8 +128,14 @@ case class Stats(nbPlayers: Int, nbGames: Int)
 // Model
 case class Player(ref: ActorRef, out: ActorRef, playerId: String)
 
-case class Game(gameId: String, player1 : Player, player2 : Option[Player], currentPlayer : Integer, lastDate : LocalDateTime, p1Duration : Duration, p2Duration : Duration) {
-  def join(player: Player) = this.copy(player2 = Some(player))
+// depending of the state, variable should be filled or not. so let put that in the design
+sealed trait Game
+case class PreGame(gameId: String, player1 : Player) extends Game {
+  def join(player2: Player) = ReadyGame(gameId, player1, player2, 1, LocalDateTime.now, Duration.ZERO, Duration.ZERO)
+}
+
+case class ReadyGame(gameId: String, player1 : Player, player2 : Player,
+                currentPlayer : Integer, lastDate : LocalDateTime, p1Duration : Duration, p2Duration : Duration) extends Game {
   def move(sowId: String) = {
     val curLDT = LocalDateTime.now
     this.copy(
@@ -135,5 +144,6 @@ case class Game(gameId: String, player1 : Player, player2 : Option[Player], curr
       p2Duration = if (this.currentPlayer == 2) {p2Duration.plus(Duration.between(lastDate, curLDT))} else p2Duration,
       lastDate = curLDT)
   }
+  def activePlayer  = if (this.currentPlayer == 1) player1 else player2
+  def passivePlayer = if (this.currentPlayer != 1) player1 else player2
 }
-
